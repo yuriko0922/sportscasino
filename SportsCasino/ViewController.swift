0aa9140a3a9c615d972a987dcb3cbecc8609b749
“yuriko0922” <yrk922@gmail.com>
2019-08-20 17:33:33 +0900

    firstcommit

---
+++ SportsCasino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsCasino
+//
+//  Created by 津國　由莉子 on 2019/08/20.
+//  Copyright © 2019 yurityann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
