92c070c4e53fe8ec0085d4937a54561cf0155e63
“yuriko0922” <yrk922@gmail.com>
2019-08-28 16:45:13 +0900

    時間のやつできたっぽい

--- SportsCasino/GameViewController.swift
+++ SportsCasino/GameViewController.swift
@@ -8,111 +8,51 @@
 
 import UIKit
 
-
-/* class DateUtils {
-    class func dateFromString(string: String, format: String) -> Date {
-        let formatter: DateFormatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.dateFormat = format
-        return formatter.date(from: string)!
-    }
-    
-    class func stringFromDate(date: Date, format: String) -> String {
-        let formatter: DateFormatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.dateFormat = format
-        return formatter.string(from: date)
-    }
-}
-
-// 元の日付の文字列
-let dateString = "2018/08/29 05:00:00 +09:00"
-
-// NSDateに変換
-let date = DateUtils.dateFromString(string: dateString, format: "yyyy/MM/dd HH:mm:ss Z")
-print(date)
-// => "2015-03-04 03:34:56 +0000\n"
-
-// Stringに再変換
-print(DateUtils.stringFromDate(date: date, format: "yyyy年MM月dd日 HH時mm分ss秒 Z"))
-// => "2015年03月04日 12時34分56秒 +0900\n"
- */
-
-
 class GameViewController: UIViewController {
-    
+    // 倍率の数字demo
     var matchRate = 2000
-    
-    
-//    myDateFormatter.dateFormat = "yyyy年M月d日 HH時mm分ss秒 Z"
-    
-//     var myDateFormatter = DateFormatter().date(from: "2019/08/29/ 12:05:00")
     
     var dictionay: [[String: Any]] = [[:]]
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-     //   var myDateFormatter = DateFormatter().date(from: "1985-04-12T23:20:50Z" )!
         
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ja_JP")
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
         dateFormatter.timeZone = TimeZone(secondsFromGMT: 9)
-        
-        let string = "1985-04-12T23:20:50Z"
+        // 試合日時１
+        let string = "2019-08-30T05:20:00Z"
         let myDate = dateFormatter.date(from: string)!
         
+        // 試合日時2
+        let strungSecond = "2019-08-30T11:00:00Z"
+        let myDateSecond = dateFormatter.date(from: strungSecond)!
+        
+        // 試合日時3
+        let stringThird = "2019-08-30T20:30:00Z"
+        let myDateThird = dateFormatter.date(from: stringThird)!
         // 試合のいろんな情報
         dictionay = [
-            ["team": "アメリカ", "team2": "日本", "day": myDate , "rate": "\(self.matchRate)"],
-            ["team": "フランス", "team2": "イタリア", "day": "2019/08/29", "time":"05:00", "rate": "\(self.matchRate)"],
-            ["team": "ブラジル", "team2": "カナダ", "day": "2019/08/31", "time":"18:00", "rate": "\(self.matchRate)"]
+            ["team": "アメリカ", "team2": "日本", "day": myDate, "rate": "\(self.matchRate)"],
+            ["team": "フランス", "team2": "イタリア", "day": myDateSecond, "rate": "\(self.matchRate)"],
+            ["team": "ブラジル", "team2": "カナダ", "day": myDateThird, "rate": "\(self.matchRate)"]
         ]
-        
-        print(myDate)
-        
-        
-        print(dictionay[1])
-        
-        
         
         // 1番上の試合
         dayLabel1.text = "\(dictionay[0]["day"] ?? "")"
-        
         teamLabel1.text = "\(dictionay[0]["team"] ?? "")"
         teamLabel21.text = "\(dictionay[0]["team2"] ?? "")"
         // 2番目の試合
         dayLabel2.text = "\(dictionay[1]["day"] ?? "")"
-        timeLabel2.text = "\(dictionay[1]["time"] ?? "")"
         teamLabel2.text = "\(dictionay[1]["team"] ?? "")"
         teamLabel22.text = "\(dictionay[1]["team2"] ?? "")"
-
-        
-        
-    
     }
-   
-
-  
-    
-    
-    
-//   var myDateFormatter1 = DateFormatter().date(from: "2018年08月30日 05時00分00秒")
-    
-    
-  //  myDateFormatter.dateFormat = "yyyy年M月d日 HH時mm分ss秒"
-//    let day1:Date = myDateFormatter.date(from: "2018年11月29日")!
- //   let day2:Date = myDateFormatter.date(from: "2018年12月2日")!
-   
-    
     
     // -------- 試合情報1 ----------------------------
     // 日付代入ラベル
     @IBOutlet weak var dayLabel1: UILabel!
-    // 時間代入ラベル
-    @IBOutlet weak var timeLabel1: UILabel!
     // チーム代入ラベル１
     @IBOutlet weak var teamLabel1: UILabel!
     // チーム代入ラベル2
@@ -147,13 +87,13 @@ class GameViewController: UIViewController {
     @IBAction func bairitu5(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-           // bairituLabel.text = "\(dictionay[0]["rate"]!)"
+            // bairituLabel.text = "\(dictionay[0]["rate"]!)"
             bairitu6.selectedSegmentIndex = 1
             bairituLabel.text = "1.5倍"
         case 1:
-           //  bairituLabel.text = "\(dictionay[0]["rate"]!)"
+            //  bairituLabel.text = "\(dictionay[0]["rate"]!)"
             bairitu6.selectedSegmentIndex = 0
-             bairituLabel.text = "50倍"
+            bairituLabel.text = "50倍"
         case 2:
             // bairituLabel.text = "\(dictionay[0]["rate"]!)"
             bairitu6.selectedSegmentIndex = 2
@@ -175,8 +115,7 @@ class GameViewController: UIViewController {
     @IBOutlet weak var teamLabel22: UILabel!
     // 日付代入ラベル
     @IBOutlet weak var dayLabel2: UILabel!
-    // 時間代入ラベル
-    @IBOutlet weak var timeLabel2: UILabel!
+
     @IBOutlet weak var bairitu7: UISegmentedControl!
     // 勝ち負け選択のやつ左
     @IBAction func bairitu3(_ sender: UISegmentedControl) {
@@ -220,21 +159,6 @@ class GameViewController: UIViewController {
         performSegue(withIdentifier: "bet", sender: nil)
     }
 }
-
-
-
-
-
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
 
 
 
